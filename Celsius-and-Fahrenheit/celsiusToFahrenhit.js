b1d0a6a2e38dc2dec3ca098176d5e74ba769b210
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-02-11 16:34:35 +0530

    Programs to convert Celsius to Fahrenhit and Fahrenhit to Celsius

---
+++ Celsius-and-Fahrenheit/celsiusToFahrenhit.js
@@ -0,0 +1,5 @@
+function celsiusToFahrenhit(cel){
+  return (cel * 9/5) + 32 ;
+}
+
+console.log(celsiusToFahrenhit(32))
---
+++ Celsius-and-Fahrenheit/fahrenhitToCelsius.js
@@ -0,0 +1,5 @@
+function fahrenhitToCelsius(fah) {
+  return (fah - 32) * 5/9;
+}
+
+console.log(fahrenhitToCelsius(98.6));
