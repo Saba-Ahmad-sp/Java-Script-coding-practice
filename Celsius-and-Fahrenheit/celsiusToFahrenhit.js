45d3e1ab81e65a2373f03319753f8fbac3769156
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-02-12 14:32:34 +0530

    Program to find longest word in a string

--- Celsius-and-Fahrenheit/celsiusToFahrenhit.js
+++ Celsius-and-Fahrenheit/celsiusToFahrenhit.js
@@ -2,4 +2,4 @@ function celsiusToFahrenhit(cel){
   return (cel * 9/5) + 32 ;
 }
 
-console.log(celsiusToFahrenhit(32))
+console.log(celsiusToFahrenhit(32));
---
+++ Longest-word-in-string/longest.js
@@ -0,0 +1,13 @@
+function longestWord(str) {
+  const words = str.split(' ');
+  let longest = '';
+
+  for(let word of words){
+    if(word.length > longest.length){
+      longest = word;
+    }
+  }
+  return longest;
+}
+
+console.log(longestWord("I am Saba Ahmad, FontEnd Developer"));
