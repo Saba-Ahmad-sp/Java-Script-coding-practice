35b4ebaa06136c69755983802b02dfbd67b25b4b
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-02-13 18:55:57 +0530

    Updated the Program for Occurance of a character in a string

--- Occurance-of-character/occurance.js
+++ Occurance-of-character/occurance.js
@@ -1,5 +1,6 @@
 function countChar(str, char){
   let count = 0;
+  str = str.toLowerCase();
   for(let i = 0; i < str.length; i++){
     if (str[i] === char){
       count ++;
--- Occurance-of-character/occuranceSplit.js
+++ Occurance-of-character/occuranceSplit.js
@@ -1,6 +1,7 @@
 function countChar(str, char){
+  str = str.toLowerCase();
   console.log(`Occurance of "${char}" in "${str}" is:`);
   return str.split(char).length - 1;
 }
 
-console.log(countChar("Saba ahmad", "a"));
+console.log(countChar("Saba Ahmad", "a"));
