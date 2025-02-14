ba01dd9f8bd1a6bdc32cdceb50783236f1c7dd3a
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-02-14 18:55:34 +0530

    Program for intersection of two arrays

---
+++ Intersection-of-Array/intersection.js
@@ -0,0 +1,14 @@
+function interSec(arr1, arr2) {
+  let newArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        newArr.push(arr1[i]);
+      }
+    }
+  }
+  return newArr;
+}
+
+console.log(interSec([1, 4, 7, 2, 5, 9], [5, 2, 6, 8, 3, 7]));
