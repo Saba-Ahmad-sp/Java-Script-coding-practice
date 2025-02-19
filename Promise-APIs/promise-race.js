ac2ab6712b7e580389b748c7f020f1d716a50285
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-02-19 22:45:34 +0530

    Program to demonstrate working of promise.race if the 1st one is fullfilled or get rejected

---
+++ Promise-APIs/promise-race.js
@@ -0,0 +1,19 @@
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("p1 success"), 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("p2 success"), 1000);
+  // setTimeout(() => reject("p2 Fail"), 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+  // setTimeout(() => resolve("p3 success"), 2000);
+    setTimeout(() => reject("p2 Fail"), 2000);
+});
+
+Promise.race([p1, p2, p3]).then((res) => {
+  console.log(res);
+}).catch((err) => {
+  console.error(err)
+});
