a6fe47675e9337793f998d893118c6128d4d4107
Saba Ahmad <sabaahmad.sp@gmail.com>
2025-02-19 16:48:46 +0530

    Promises chaining methods and handling the error usig catch

--- Promises/promise.js
+++ Promises/promise.js
@@ -1,13 +1,27 @@
 const cart = ["shirt", "pant", "tshirt", "kurta"];
 
-const promise = createOrder(cart);
+// const promise = createOrder(cart);
 
-promise.then(function (orderId) {
-  console.log(orderId);
-}).catch(function (err) {
-  console.log(err.message);
-});
+// promise.then(function (orderId) {
+//   console.log(orderId);
+// }).catch(function (err) {
+//   console.log(err.message);
+// });
 
+createOrder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+  })
+  .then(function (orderId) {
+   return proceedToPayments(orderId);
+  })
+  .then(function (paymentInfo) {
+    console.log(paymentInfo);
+  })
+  .catch(function (err) {
+    console.log(err.message);
+  });
 
 //producer
 function createOrder(cart) {
@@ -15,18 +29,23 @@ function createOrder(cart) {
     //createOrder
     //validateCart
     //orderId
-    if(!validateCart(cart)){
+    if (!validateCart(cart)) {
       const err = new Error("Cart is not valid");
       reject(err);
     }
     const orderId = "12345";
-    if(orderId){
-      setTimeout(function(){
+    if (orderId) {
+      setTimeout(function () {
         resolve(orderId);
       }, 3000);
     }
   });
   return pr;
+}
+function proceedToPayments(orderId) {
+  return new Promise(function (resolve, reject) {
+    resolve("Payment Successful");
+  });
 }
 
 function validateCart(cart) {
